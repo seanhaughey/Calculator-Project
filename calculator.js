446fd7733d2096ab082f95098d595677b9620a97
Akash Shekara <akashshekara1212@gmail.com>
2015-09-04 10:34:57 -0500

    Added add button functionality

--- calculator.js
+++ calculator.js
@@ -8,10 +8,11 @@ function(){
 	//then the number in the input box needs to clear out 
 	//the input after the box clears needs to be saved in another variable 
 	
-		$("/* whatever the button id is */").on("click",function(){
-			//input 1 + input 2
-		}
-	
-
-		
+	$("#add").on("click",function(){
+		//input 1 + input 2
+		var num1 = parseInt($("#inputNum1").val());
+		var num2 = parseInt($("#inputNum2").val());
+		var sum = num1 + num2;
+		console.log(num1+num2);
+	}	
 }
